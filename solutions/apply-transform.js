51317cea427f63d60205c51988001a24efa91c76
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2023-05-08 10:57:28 +0530

    Implemented without map fn

--- solutions/apply-transform.js
+++ solutions/apply-transform.js
@@ -4,9 +4,14 @@
  * @return {number[]}
  */
 var map = function (arr, fn) {
-  return arr.map(fn);
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(fn(arr[i], i));
+  }
+  return res;
 };
 
+console.log(map([1, 2, 3]));
 // Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
 
 // The returned array should be created such that returnedArray[i] = fn(arr[i], i).
